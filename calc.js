47f5654ba36c5c1c0d7217fd93633f1e8cf17337
Adam Solove <asolove@gmail.com>
2013-05-02 21:08:42 -0400

    Make the calculation more interesting.

--- calc.js
+++ calc.js
@@ -2,18 +2,44 @@
 "use strict";
 
 var vars = {};
-var revenue = vars.revenue = new c.Variable({ name: "revenue" });
-var outlays = vars.outlays = new c.Variable({ name: "outlays" });
+var revenue = vars.revenue = new c.Variable({ name: "revenue", value: 3464 });
+var outlays = vars.outlays = new c.Variable({ name: "outlays", value: 3464 });
 var balance = vars.balance = new c.Variable({ name: "balance" });
+
+var taxRates = [10, 15, 25, 28, 33, 35, 39.6];
+// FIXME: real numbers
+var dollarsInBracket = [2, 4, 6, 8, 10, 20, 50];
+
+taxRates.forEach(function(rate, i){
+	vars["tax-bracket-"+i] = new c.Variable({ name: "tax-bracket-"+i, value: rate });
+})
+
 
 var constraints = [
 	new c.Equation(c.minus(revenue, outlays), balance, c.Strength.required, 0),
 	new c.Inequality(revenue, c.GEQ, 0, c.Strength.required, 0),
 	new c.Inequality(outlays, c.GEQ, 0, c.Strength.required, 0),
-	new c.StayConstraint(revenue, c.Strength.medium, 0),
-	new c.StayConstraint(outlays, c.Strength.medium, 0),
-	new c.StayConstraint(balance, c.Strength.medium, 0)
+	new c.StayConstraint(outlays, c.Strength.medium, 0)
 ];
+
+var taxRevenue = new c.Expression(0);
+taxRates.forEach(function(rate, i){
+	var rate = vars["tax-bracket-"+i];
+	var nextRate = vars["tax-bracket-"+(i+1)] || 100;
+	console.log(rate.value, nextRate.value || nextRate);
+
+	constraints.splice(0,0,
+		new c.Inequality(rate, c.LEQ, nextRate, c.Strength.required, 0),
+		new c.Inequality(rate, c.GEQ, 0, c.Strength.required, 0),
+		new c.StayConstraint(rate, c.Strength.medium, 0)
+	);
+
+	taxRevenue = (new c.Expression(rate)).times(dollarsInBracket[i]).plus(taxRevenue);
+});
+
+constraints.push(
+	new c.Equation(taxRevenue, vars.revenue, c.Strength.required, 0)
+);
 
 var s = c.extend(new c.SimplexSolver(), {
 	onsolved: function(){
@@ -23,12 +49,6 @@ var s = c.extend(new c.SimplexSolver(), {
 
 constraints.forEach(function(c){ s.addConstraint(c); });
 s.resolve();
-
-
-s.addEditVar(revenue, c.Strength.high).beginEdit()
- .suggestValue(revenue, 100)
- .resolve()
-s.endEdit();
 
 
 var views = document.querySelectorAll("[data-variable]");
@@ -79,10 +99,22 @@ function attachControls(){
 
 }
 
+var formatters = {
+	percent: function(v){
+		return (v).toFixed(0) + "%";
+	},
+
+	dollars: function(v){
+		return "$" + (v.toFixed(2));
+	}
+};
+
+var id = function(a){ return a; };
+
 function render(){
 	_.each(viewForVar, function(el, varName){
 		var variable = vars[varName];
-		el.innerHTML = "$" + variable.value;
+		el.innerHTML = (formatters[el.dataset.format]||id)(variable.value);
 	});
 }
 
